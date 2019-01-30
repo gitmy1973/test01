c7b731fdc56c30dcbca0babc30474582be0d7d28
gitmy1973 <m_yama_work@yahoo.co.jp>
2019-01-30 11:53:52 +0900

    ３回目のバージョン履歴

--- Exec.java
+++ Exec.java
@@ -1,5 +1,7 @@
 public class Exec{
 	public static void main(String[] args){
-		System.out.println("Git練習バージョン管理");
+		System.out.println("Git練習");
+		System.out.println("バージョン履歴");
+		System.out.println("2回目の履歴記録中");
 	}
 }
